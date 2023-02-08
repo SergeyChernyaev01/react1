91366c646e9b699db24ceab471ea683fd2f3133b
SergeyChernyaev01 <123984449+SergeyChernyaev01@users.noreply.github.com>
2023-02-08 18:01:58 +0000

    Tesak

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+     
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+      <h1>Окупай педофиляй.</h1>
+        <img src="https://i.gifer.com/origin/27/27dc29a27242bc075142197b4c5f9416_w200.webp" className="App-logo" alt="logo" width='1920px'/>
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Влад  <span className="heart">♥️</span> Детское порно !
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+        <img src="https://i.gifer.com/origin/31/316d9c73bc6bb7e2f2fd6fde925f64ed_w200.webp"/>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://sun9-83.userapi.com/impg/LlfVI3LtiI1yGwB0LZ668b5f7X_XCb92HOyVkA/I5En0AzJtdI.jpg?size=912x606&quality=96&sign=85199d5173a68038c0e835e1e7b86a05&c_uniq_tag=bimvbQEilixv5b_LikbLj47ZWU2nkl6VskhGUU5aTqo&type=album"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Смотреть
           </a>
         </p>
       </header>
